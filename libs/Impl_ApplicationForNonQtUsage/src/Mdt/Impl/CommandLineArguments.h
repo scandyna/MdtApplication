4b220e9859faf9bb8b6e93173da8170a5ddc1a51
Philippe Steinmann <philippe.steinmann@gmail.com>
2020-04-14 17:39:59 +0200

    Fix comparison with size_t warning

--- libs/Impl_ApplicationForNonQtUsage/src/Mdt/Impl/CommandLineArguments.h
+++ libs/Impl_ApplicationForNonQtUsage/src/Mdt/Impl/CommandLineArguments.h
@@ -49,7 +49,7 @@ namespace Mdt{ namespace Impl{
      : mArgc(args.size()),
        mIsCopy(true)
     {
-      assert(args.size() >= std::size_t{1});
+      assert(static_cast<int>(args.size()) >= 1);
 
       mArgv = new char*[mArgc+1];
       int row = 0;

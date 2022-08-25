ed8c646a88ff29232b3aca63c8d9256ce6778c19
Philippe Steinmann <philippe.steinmann@gmail.com>
2022-08-25 11:09:06 +0200

    Doc CMakeDependencyGraph: ignore Conan generated targets

--- CMakeGraphVizOptions.cmake
+++ CMakeGraphVizOptions.cmake
@@ -1,2 +1,6 @@
 
 set(GRAPHVIZ_EXTERNAL_LIBS False)
+
+# Using Conan CMakeDeps generator will create some targets
+# that are not recognized as external libraries
+set(GRAPHVIZ_IGNORE_TARGETS "^[a-z]" "^MdtCMakeModules")
